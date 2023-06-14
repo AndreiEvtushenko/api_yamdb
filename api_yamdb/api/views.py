8dac938e0b9fe3ef950f2d12e92504e914c03923
Рябов Павел <ryabovpavel@Rabovs-MacBook-Air.local>
2023-06-14 11:42:10 +0300

    Пермишены

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from api.serializers import (CommentSerializer, Categories,
+                             GroupSerializer, PostSerializer)
+from posts.models import Follow, Group, Post
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsUserOrAuthorOrReadOnly,)
+
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        return self.get_post().comments
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post())
+
+
+class LightFollowViewSet(CreateListViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_fields = ('user',)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return Follow.objects.select_related('following').all().filter(
+            user=self.request.user
+        )
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsUserOrAuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
