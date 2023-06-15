52f05ced1d834e7fc9b824d557d29660479af83d
AndreiEvtushenko <104425601+AndreiEvtushenko@users.noreply.github.com>
2023-06-15 15:30:18 +0700

    task13/admin (#12)

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -3,3 +3,18 @@ from django.contrib import admin
 from .models import User
 
 admin.site.register(User)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role'
+    )
+    search_fields = ('username',)
+    list_filter = ('pub_date',)
+    empty_value_display = 'пусто'
