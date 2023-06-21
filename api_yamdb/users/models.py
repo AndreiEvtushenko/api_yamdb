f9988de701b59c21baa8286bcfbea610e58ca238
Andrei <evtushenkoad@gmail.com>
2023-06-21 20:25:17 +0300

    feature/TestUser

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -14,6 +14,21 @@ ROLES = [
 
 
 class User(AbstractUser):
+    first_name = models.CharField(
+        verbose_name='Имя пользователя',
+        help_text='Введите имя пользователя',
+        max_length=150,
+        blank=True)
+    last_name = models.CharField(
+        verbose_name='Фамилия пользователя',
+        help_text='Введите фамилию пользователя',
+        max_length=150,
+        blank=True)
+    email = models.EmailField(
+        blank=False,
+        max_length=254,
+        unique=True
+        )
     username = models.CharField(
         verbose_name='Уникальное имя пользователя',
         help_text='Введите username',
@@ -36,10 +51,10 @@ class User(AbstractUser):
         help_text='Можно выбрать из трех: "user" "moderator" "admin"',
         choices=ROLES,
         default='user',
-        blank=False
+        blank=True
     )
     confirmation_code = models.CharField(
-        max_length=10,
+        max_length=250,
         null=True,
         blank=True
     )
